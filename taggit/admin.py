86c40a8910d4c847155b93b591f8349940c0dd35
Eden Redman <46800990+Existentialist-Robot@users.noreply.github.com>
2020-02-16 11:47:39 -0700

    Add files via upload

---
+++ taggit/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from taggit.models import Tag, TaggedItem
+
+
+class TaggedItemInline(admin.StackedInline):
+    model = TaggedItem
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    inlines = [TaggedItemInline]
+    list_display = ["name", "slug"]
+    ordering = ["name", "slug"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ["name"]}
+
+
+class MyModelAdmin(admin.ModelAdmin):
+    list_display = ['tag_list']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
